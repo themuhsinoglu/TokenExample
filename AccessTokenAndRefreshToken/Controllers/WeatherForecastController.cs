63da450279a315882cffa4f7f45511ae12cc3aa6
ahmet.muhsinoglu <ahmet.muhsinoglu@madamecoco.com>
2023-04-13 10:55:04 +0300

    finished

--- AccessTokenAndRefreshToken/Controllers/WeatherForecastController.cs
+++ AccessTokenAndRefreshToken/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccessTokenAndRefreshToken.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
